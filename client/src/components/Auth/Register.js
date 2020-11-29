a499a62b004c37d69b0be0338448f0d9ed52d3fe
nicksandoval <sand8val@gmail.com>
2020-11-28 21:36:39 -0800

    refactored reg

--- client/src/components/Auth/Register.js
+++ client/src/components/Auth/Register.js
@@ -5,6 +5,7 @@ import axios from 'axios'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import ProgressBar from 'react-bootstrap/ProgressBar'
+import Col from 'react-bootstrap/Col'
 
 
 
@@ -12,20 +13,27 @@ export default function Register() {
 
     const { setUserData } = useContext(UserContext);
     const [ progress, setProgress ] = useState(1)
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
-    const [passwordCheck, setPasswordCheck] = useState();
-    const [userName, setUserName] = useState();
-    
-    
+    const [ userRegistration, setUserRegistration ] = useState({
+        email: '',
+        password: '',
+        passwordCheck: '',
+        userName: '',
+        firstName: '',
+        lastName: '',
+        addressOne: '',
+        addressTwo: '',
+        city: '',
+        state: '',
+        zip: ''
 
+    }) 
 
-
+  
     const history = useHistory();
 
     const submit = async (e) => {
         e.preventDefault()
-        console.log('hihihi')
+        console.log(userRegistration)
     // try {
 
     //     const newUser = {email, password, passwordCheck, userName }
@@ -54,70 +62,23 @@ export default function Register() {
     
     let regProgress;
 
-    if (progress === 1) {
+    if (progress === 2) {
         regProgress = (
             <div>
-                <ProgressBar now={50} variant="secondary"/>
-            
-                <Form.Group className="mt-5">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control 
-                        type="email" 
-                        placeholder="Enter email" 
-                        id='register-email' 
-                        onChange={e => setEmail(e.target.value)}
-                    />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control 
-                        type="password" 
-                        autoComplete="on" 
-                        placeholder="Password" 
-                        id='register-password' 
-                        onChange={e => setPassword(e.target.value)}
-                    />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Confirm Password</Form.Label>
-                    <Form.Control 
-                        type="password" 
-                        autoComplete="on"
-                        placeholder='Verify password' 
-                        onChange={e => setPasswordCheck(e.target.value)} 
-                    />
-                </Form.Group>
-                {/* <Form.Group>
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control 
-                        type="text" 
-                        placeholder="" 
-                        id='register-display-name' 
-                        onChange={e => setUserName(e.target.value)}
-                    />
-                </Form.Group> */}
-                <Button 
-                    variant="primary" 
-                    type="submit"
-                    className="pr-4 pl-4 btn-dark mt-3 ml-auto" 
-                    onClick={() => setProgress(2)}
-                >Next</Button>
-            
-            </div>
-        )
-    } else if (progress === 2) {
-        regProgress = (
-                
-                <div>
                 <ProgressBar now={100} variant="secondary"/>
-                
+            
                 <Form.Group className="mt-5">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control 
                         type="email" 
                         placeholder="Enter email" 
                         id='register-email' 
-                        onChange={e => setEmail(e.target.value)}
+                        onChange={e => {
+                            setUserRegistration({
+                                ...userRegistration,
+                                email: e.target.value
+                            })
+                        }}
                     />
                 </Form.Group>
                 <Form.Group>
@@ -127,7 +88,12 @@ export default function Register() {
                         autoComplete="on" 
                         placeholder="Password" 
                         id='register-password' 
-                        onChange={e => setPassword(e.target.value)}
+                        onChange={e => {
+                            setUserRegistration({
+                                ...userRegistration,
+                                password: e.target.value
+                            })
+                        }}
                     />
                 </Form.Group>
                 <Form.Group>
@@ -136,30 +102,152 @@ export default function Register() {
                         type="password" 
                         autoComplete="on"
                         placeholder='Verify password' 
-                        onChange={e => setPasswordCheck(e.target.value)} 
+                        onChange={e => {
+                            setUserRegistration({
+                                ...userRegistration,
+                                passwordCheck: e.target.value
+                            })
+                        }}
                     />
                 </Form.Group>
-                {/* <Form.Group>
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control 
-                        type="text" 
-                        placeholder="" 
-                        id='register-display-name' 
-                        onChange={e => setUserName(e.target.value)}
-                    />
-                </Form.Group> */}
+
                 <Button 
                     variant="primary" 
                     type="submit"
-                    className="pr-4 pl-4 btn-dark mt-3 ml-auto" 
+                    className="pr-4 pl-4 btn-dark mt-3" 
                     onClick={() => setProgress(1)}
                 >Back</Button>
                 <Button 
                     variant="primary" 
                     type="submit"
-                    className="pr-4 pl-4 btn-dark mt-3 ml-auto" 
+                    className="pr-4 pl-4 btn-dark mt-3 float-right" 
                 >Create</Button>
+                
+                
             
+            </div>
+        )
+    } else if (progress === 1) {
+        regProgress = (
+                
+                <div>
+                <ProgressBar now={50} variant="secondary"/>
+                
+                    <Form.Row className="mt-5">
+                        <Form.Group as={Col} >
+                        <Form.Label>First Name</Form.Label>
+                        <Form.Control
+                            onChange={e => {
+                                setUserRegistration({
+                                    ...userRegistration,
+                                    firstName: e.target.value
+                                })
+                            }}
+                        />
+                        </Form.Group>
+
+                        <Form.Group as={Col} >
+                        <Form.Label>Last Name</Form.Label>
+                        <Form.Control 
+                            onChange={e => {
+                                setUserRegistration({
+                                    ...userRegistration,
+                                    lastName: e.target.value
+                                })
+                            }}
+                        />
+                        </Form.Group>
+
+                        <Form.Group as={Col}>
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control 
+                            onChange={e => {
+                                setUserRegistration({
+                                    ...userRegistration,
+                                    userName: e.target.value
+                                })
+                            }}
+                        />
+                        </Form.Group>
+                    </Form.Row>
+
+                    
+
+                    <Form.Group >
+                        <Form.Label>Address</Form.Label>
+                        <Form.Control
+                            onChange={e => {
+                                setUserRegistration({
+                                    ...userRegistration,
+                                    addressOne: e.target.value
+                                })
+                            }}
+                        />
+                    </Form.Group>
+
+                    <Form.Group >
+                        <Form.Label>Address 2</Form.Label>
+                        <Form.Control  
+                            onChange={e => {
+                                setUserRegistration({
+                                    ...userRegistration,
+                                    addressTwo: e.target.value
+                                })
+                            }}
+                        />
+                    </Form.Group>
+
+                    <Form.Row>
+                        <Form.Group as={Col} >
+                        <Form.Label>City</Form.Label>
+                        <Form.Control 
+                            onChange={e => {
+                                setUserRegistration({
+                                    ...userRegistration,
+                                    city: e.target.value
+                                })
+                            }}
+                        />
+                        </Form.Group>
+
+                        <Form.Group as={Col} >
+                        <Form.Label>State</Form.Label>
+                        <Form.Control 
+                            as="select" 
+                            defaultValue="Choose..."
+                            onChange={e => {
+                                setUserRegistration({
+                                    ...userRegistration,
+                                    state: e.target.value
+                                })
+                            }}
+                        >
+                            <option>Choose...</option>
+                            <option>California</option>
+                        </Form.Control>
+                        </Form.Group>
+
+                        <Form.Group as={Col} >
+                        <Form.Label>Zip</Form.Label>
+                        <Form.Control 
+                            onChange={e => {
+                                setUserRegistration({
+                                    ...userRegistration,
+                                    zip: e.target.value
+                                })
+                            }}
+                        />
+                        </Form.Group>
+                    </Form.Row>
+
+                    <Button 
+                    variant="primary" 
+                    type="submit"
+                    className="pr-4 pl-4 btn-dark mt-3 ml-auto" 
+                    onClick={() => setProgress(2)}
+                    >Next</Button>
+
+ 
             </div>
         )
     }
@@ -168,7 +256,7 @@ export default function Register() {
     <div className='register-login-wrapper bg-light mt-5'>
         
         <div className='register-login-div'>
-            <h1 className="text-center text-light register-title">Traveler Registration</h1>
+            <h1 className="text-center text-light register-title border-bottom border-info">Traveler Registration</h1>
             <div className='register-login-form'>
             <Form onSubmit={submit} className="mt-5">
             {regProgress}
