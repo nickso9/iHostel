c51a9ab0ec89ef60c4098a365c8fa6799e954764
nicksandoval <sand8val@gmail.com>
2020-12-12 17:28:50 -0800

    asd

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 5000
 
-const URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_DATABASE}`;
-mongoose.connect( URI, 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_DATABASE}`;
+mongoose.connect( uri, 
     { 
         useNewUrlParser: true, 
         useUnifiedTopology: true , 
